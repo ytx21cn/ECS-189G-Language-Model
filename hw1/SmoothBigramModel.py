d59e25739d9fe6d62645b579a3126dcb51c5e727
ytx21cn <ytxmobile@outlook.com>
2019-04-22 18:40:04 -0700

    successfully implemented smooth bigram model

--- hw1/SmoothBigramModel.py
+++ hw1/SmoothBigramModel.py
@@ -12,22 +12,30 @@ class SmoothBigramModel:
     """ Takes a corpus and trains your language model. 
         Compute any counts or other corpus statistics in this function.
     """  
-    # TODO your code here
+    # COMPLETED your code here
     # Tip: To get words from the corpus, try
     #    for sentence in corpus.corpus:
     #       for datum in sentence.data:  
     #         word = datum.word
 
     for sentence in corpus.corpus:
-      for datum in sentence.data:
-        token = datum.word
+      for i in xrange(1, len(sentence.data)):
+        token = (sentence.data[i].word, sentence.data[i-1].word) # first entry is current word; second entry is previous word
         self.smoothBigramCounts[token] += 1
         self.total += 1
-    
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
-    # TODO your code here
-    return 0.0
+    # COMPLETED your code here
+
+    score = 0.0
+
+    for i in xrange(1, len(sentence)):
+      token = (sentence[i], sentence[i-1])
+      count = self.smoothBigramCounts[token] + 1
+      score += math.log(count)
+      score -= math.log(self.total)
+    
+    return score
